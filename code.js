d1b36b45d74729d686beadb81e0ee02326ee3121
DanYan97 <158104062+DanYan97@users.noreply.github.com>
2024-10-15 19:03:53 +0000

    First Update

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
+function mergeInplace(array, lo, mid, hi) {
+    var i = lo;
+    var j = mid + 1;
+
+    while ( i <= mid && j <= hi) {
+        if (array[i] <= array[j]) {
+            i++;
+        } else {
+            var value = array[j];
+
+            // shift elements by 1
+            for (var index = j; index > i; index--) {
+                array[index] = array[index - 1];
+            }
+            array[i] = value;
+
+            // continue merging
+            i++;
+            mid++;
+            j++;
+        }
+    }
+}
+
 function mergesort(array) {
+
+    var n = array.length;
+    var size;
+
+    // start with size 1 then increase the size by double each iteration
+    for (size = 1; size < n; size *= 2) {
+        for (var lo = 0; lo < n - size; lo += 2 * size) {
+            var mid = lo + size - 1;
+            var hi = Math.min(lo + 2 * size - 1, n - 1);
+
+            // merge the subarrays in-place
+            mergeInplace(array, lo, mid, hi);
+        }
+    }
     return array;
 }
